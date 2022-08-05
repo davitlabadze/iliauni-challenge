c7bf1301cf05eef8bf60d78c8246ef19cb11cd4c
davitlabadze <labadzedato18@gmail.com>
2022-08-05 15:32:20 +0400

    style: create posts page

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Blog</title>
+</head>
+
+<body>
+    <div class="container  mx-auto grid grid-cols-4 ">
+        @foreach($posts as $post)
+        <div class=" p-12 mt-10 rounded-lg mb-10 duration-300 scale-95 hover:scale-100 shadow-lg">
+            <div class="underline">
+                <h2>{{ $post->title }}</h2>
+            </div>
+            <div class="mt-4 mb-12">
+                <h2>{{ substr($post->body ,0,50) }}...</h2>
+            </div>
+            <div class="bottom-4 fixed">
+                <button class="bg-blue-600 hover:bg-blue-700 rounded p-2 mt-10  text-white">See more</button>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</body>
+
+</html>
