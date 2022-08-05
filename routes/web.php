c49abea69f36a4864164fa7a36c2fa29c837f2b2
davitlabadze <labadzedato18@gmail.com>
2022-08-05 17:45:48 +0400

    refactor(routes): add route name

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,5 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('index');
-// });
-Route::get('/', [PostController::class, 'posts']);
-Route::get('/post/{id}', [PostController::class, 'post']);
+Route::get('/', [PostController::class, 'posts'])->name('posts');
+Route::get('/post/{id}', [PostController::class, 'post'])->name('post');
