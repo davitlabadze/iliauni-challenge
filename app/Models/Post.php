89a1ca6f842d30536c69922cad7c24012d216b8f
davitlabadze <labadzedato18@gmail.com>
2022-08-05 12:52:16 +0400

    feat: create post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    /**
+     * Relationship: users
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
