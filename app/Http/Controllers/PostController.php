21ecf2e46ba0d9298b868113b988797c335e9a11
davitlabadze <labadzedato18@gmail.com>
2022-08-05 15:33:34 +0400

    feat: create post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    /**
+     * Get Posts
+     *
+     * @return object
+     */
+    public function posts()
+    {
+        $posts = Post::get();
+        return view('posts', ['posts' => $posts]);
+    }
 }
